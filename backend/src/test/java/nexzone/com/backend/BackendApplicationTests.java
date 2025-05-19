06d50c5454fafac3be2e2933e03b5d91081b3a24
Le Dinh Duc <ledinhduc1879@gmail.com>
2025-05-19 21:01:04 +0700

    Initial commit

---
+++ backend/src/test/java/nexzone/com/backend/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package nexzone.com.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
